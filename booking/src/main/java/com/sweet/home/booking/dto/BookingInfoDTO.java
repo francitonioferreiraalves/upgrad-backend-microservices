f763444989851f7b1b648beb379bf073408df0d2
tito <tito@pop-os.localdomain>
2023-08-25 14:15:10 -0300

    first commit

---
+++ booking/src/main/java/com/sweet/home/booking/dto/BookingInfoDTO.java
@@ -0,0 +1,100 @@
+package com.sweet.home.booking.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class BookingInfoDTO {
+
+    private int bookingId;
+
+    private String fromDate;
+
+    private String toDate;
+
+    private String aadharNumber;
+
+    @NotBlank(message = "numOfRooms cannot be null")
+    private int numOfRooms;
+
+    @NotBlank(message = "roomNumbers cannot be null")
+    private String roomNumbers;
+
+    @NotBlank(message = "roomPrice cannot be null")
+    private int roomPrice;
+
+    @NotBlank(message = "transactionId cannot be null")
+    private int transactionId;
+
+    private String bookedOn;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getAadharNumber() {
+        return aadharNumber;
+    }
+
+    public void setAadharNumber(String aadharNumber) {
+        this.aadharNumber = aadharNumber;
+    }
+
+    public int getNumOfRooms() {
+        return numOfRooms;
+    }
+
+    public void setNumOfRooms(int numOfRooms) {
+        this.numOfRooms = numOfRooms;
+    }
+
+    public String getRoomNumbers() {
+        return roomNumbers;
+    }
+
+    public void setRoomNumbers(String roomNumbers) {
+        this.roomNumbers = roomNumbers;
+    }
+
+    public int getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(int roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getBookedOn() {
+        return bookedOn;
+    }
+
+    public void setBookedOn(String bookedOn) {
+        this.bookedOn = bookedOn;
+    }
+}
